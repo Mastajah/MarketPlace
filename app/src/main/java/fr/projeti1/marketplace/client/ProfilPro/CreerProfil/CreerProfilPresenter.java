c6c8ec94cf5cd8f059dda9082fb8f05045167cf7
gbesombes <guillaume.besombes@gmail.com>
2018-06-05 12:03:11 +0200

    CreerProfilProCodeV1

--- app/src/main/java/fr/projeti1/marketplace/client/ProfilPro/CreerProfil/CreerProfilPresenter.java
+++ app/src/main/java/fr/projeti1/marketplace/client/ProfilPro/CreerProfil/CreerProfilPresenter.java
@@ -1,4 +1,23 @@
 package fr.projeti1.marketplace.client.ProfilPro.CreerProfil;
 
-public class CreerProfilPresenter {
+import fr.projeti1.marketplace.client.MVPPattern.ActivityPresenter;
+
+public class CreerProfilPresenter extends ActivityPresenter {
+    protected CreerProfil createProfilView;
+    protected CreerProfilModel createProfilModel;
+
+    public CreerProfilPresenter (CreerProfil vue){
+        super(vue);
+        initPresenter();
+    }
+
+    private void initPresenter(){
+        createProfilModel = new CreerProfilModel();
+        createProfilView.initView();
+    }
+
+    public void onClick(android.view.View view){
+        //remplir ici
+    }
+
 }
