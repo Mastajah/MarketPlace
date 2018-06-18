b8931b1f33edcd5000a930503d574dbb5bb255ea
Mastajah <emilienrivals@orange.fr>
2018-06-18 19:39:41 +0200

    ERI Push

---
+++ app/src/main/java/fr/projeti1/marketplace/server/DAO/AnnonceDAO.java
@@ -0,0 +1,7 @@
+package fr.projeti1.marketplace.server.DAO;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface AnnonceDAO {
+}
---
+++ app/src/main/java/fr/projeti1/marketplace/server/service/AnnonceService.java
@@ -0,0 +1,6 @@
+package fr.projeti1.marketplace.server.service;
+
+import android.arch.persistence.room.RoomDatabase;
+
+public class AnnonceService {
+}
